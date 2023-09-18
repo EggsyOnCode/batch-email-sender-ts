5dce9d21a117e2de0a74982ae0967626cdc2b2ca
EggsyOnCode <farzansaqib2003@gmail.com>
2023-09-18 19:58:24 +0500

    ui running; email going; no err handling tho ; for later

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -2,7 +2,9 @@ import { useAuth0 } from "@auth0/auth0-react";
 import React, { useState, useReducer } from "react";
 import { reducer } from "../hooks/useReducer";
 import AddIcon from "@mui/icons-material/Add";
+import { useAppSelector } from "../hooks/Redux";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export interface Email {
   id: number;
@@ -14,10 +16,11 @@ export interface Email {
 const emails: Email[] = [{ id: 0, to: "", subject: "", email: "" }];
 
 const Dashboard: React.FC = () => {
+  const appkey = useAppSelector((state) => state.keySlice.value);
   const [count, setCount] = useState(1);
   const { logout, user } = useAuth0(undefined);
   const navigate = useNavigate();
-  const goToConfig = () => navigate("/config", { replace: true });
+  const goToConfig = () => navigate("/config");
   const [state, dispatch] = useReducer(reducer, { emails });
 
   // state handlers
@@ -26,7 +29,6 @@ const Dashboard: React.FC = () => {
       setCount((prevState) => prevState + 1);
       dispatch({ type: "add", payload: { id: count } });
       console.log(count);
-      
     } else if (action === "-") {
       setCount((prevState) => prevState - 1);
     }
@@ -36,7 +38,19 @@ const Dashboard: React.FC = () => {
     handleCount("-");
     dispatch({ type: "discard", payload: { id: ID } });
   }
-  
+
+  async function sendEmail(to: string, subject: string, content: string) {
+    const req = { from: user?.email, to: to, subject: subject, email: content, key: appkey };
+    const response = await axios.post("http://localhost:3000/sendEmail", req);
+    if(response.status == 200){
+      console.log("success");
+      
+    }else{
+      console.log("failure!");
+      
+    }
+  }
+
   return (
     <div className="min-h-screen w-screen bg-black flex-col items-center justify-center">
       <section className="navbar h-[45px] bg-blue-950 w-screen flex justify-center">
@@ -51,7 +65,12 @@ const Dashboard: React.FC = () => {
       </section>
 
       <section className="counter pt-10 flex justify-center items-center hover:cursor-pointer">
-        <h1 className="text-[14px] text-white mr-6 hover:text-orange-300" onClick={()=>{goToConfig()}}>
+        <h1
+          className="text-[14px] text-white mr-6 hover:text-orange-300"
+          onClick={() => {
+            goToConfig();
+          }}
+        >
           <i>Learn How to Setup Email Sending feature?</i>
         </h1>
         <div
@@ -70,18 +89,30 @@ const Dashboard: React.FC = () => {
                 type="text"
                 placeholder="To:"
                 className="bg-white text-black mb-2 w-[350px] rounded-md"
+                onChange={(e) => {
+                  emails[index].to = e.target.value;
+                }}
               />
               <input
                 type="text"
                 placeholder="Subject:"
                 className="bg-white text-black mb-2 w-[350px] rounded-md"
+                onChange={(e) => {
+                  emails[index].subject = e.target.value;
+                }}
               />
               <input
                 placeholder="Message:"
                 className="bg-white text-black h-[200px] w-[340px] md:w-[400px] lg:w-[450px] rounded-md"
+                onChange={(e) => {
+                  emails[index].email = e.target.value;
+                }}
               />
               <div className="flex flex-row mt-2">
-                <button className="mr-40 bg-green-800 rounded-xl p-2">
+                <button
+                  className="mr-40 bg-green-800 rounded-xl p-2"
+                  onClick={() => sendEmail(obj.to, obj.subject, obj.email)}
+                >
                   Send
                 </button>
                 <button
@@ -98,7 +129,6 @@ const Dashboard: React.FC = () => {
           );
         })}
       </section>
-      
     </div>
   );
 };
